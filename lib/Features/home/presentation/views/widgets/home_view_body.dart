7cce547733f6698d20a412b7eed1b7d9a35b2617
Shehata <shehata@gamil.com>
2025-01-31 15:09:16 +0200

    best seller list view part one

--- lib/Features/home/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/presentation/views/widgets/home_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly/Features/home/presentation/views/widgets/custome_app_bar.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custome_list_view_item.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/featured_list_view.dart';
+import 'package:bookly/core/utils/assets.dart';
 import 'package:bookly/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -30,3 +31,34 @@ class HomeViewBody extends StatelessWidget {
   }
 }
 
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [
+      AspectRatio(
+        aspectRatio: 2.5 / 4,
+        child: Container(
+          height: MediaQuery.of(context).size.height * .25,
+          width: 100,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(8),
+            image: const DecorationImage(
+              image: AssetImage(AssetsData.testImage),
+              fit: BoxFit.cover,
+            ),
+          ),
+        ),
+      ),
+      Column(
+        children: [
+        
+      ]),
+      ]),
+    );
+  }
+}
