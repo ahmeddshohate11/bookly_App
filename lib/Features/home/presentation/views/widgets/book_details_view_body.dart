61679ea5cabc02080ca11b1742eb87843c97136a
Shehata <shehata@gamil.com>
2025-02-06 10:54:35 +0200

    add title and sub title to details

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,5 +1,6 @@
 import 'package:bookly/Features/home/presentation/views/widgets/custome_book_details_app_bar.dart';
 import 'package:bookly/Features/home/presentation/views/widgets/custome_book_item.dart';
+import 'package:bookly/core/utils/styles.dart';
 
 import 'package:flutter/material.dart';
 
@@ -15,8 +16,31 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           CustomeBookDetailsAppBar(),
           Padding(
-              padding: EdgeInsets.symmetric(horizontal: width * .16),
+              padding: EdgeInsets.symmetric(horizontal: width * .2),
               child: CustomeBookImage()),
+              const SizedBox(
+                height: 43,
+              ),
+              Text('The Jungle Book ',
+              style: Styles.textStyle30.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+              ),
+              const SizedBox(
+                height: 6,
+              ),
+              Opacity(
+                opacity: .7,
+                child: Text('Rudyard Kipling',
+                style: Styles.textStyle18.copyWith(
+                  fontStyle: FontStyle.italic,
+                  fontWeight: FontWeight.w500,
+
+
+                ),
+                ),
+              ),
+
         ],
       ),
     );
--- lib/Features/home/presentation/views/widgets/featured_list_view.dart
+++ lib/Features/home/presentation/views/widgets/featured_list_view.dart
@@ -14,6 +14,7 @@ class FeaturedBooksdListView extends StatelessWidget {
             return const Padding(
               padding: EdgeInsets.symmetric(horizontal: 20),
               child: CustomeBookImage(),
+              
             );
           }),
     );
