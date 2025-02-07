264131dfb10001bf764a472e4c549a084b1b894b
Shehata <shehata@gamil.com>
2025-02-07 17:18:03 +0200

    create api service

--- lib/Features/home/data/repos/home_repo.dart
+++ lib/Features/home/data/repos/home_repo.dart
@@ -1,4 +1,6 @@
+import 'package:bookly/Features/home/data/models/book_model/book_model.dart';
+
 abstract class HomeRepo {
- fetchBestSellerBooks();
- fetchFeaturedBooks();
- }
+ Future<List<BookModel>>fetchBestSellerBooks();
+  Future<List<BookModel>>fetchFeaturedBooks();
+ } 
